17d6c5a6ceeebd2f232cdf340abefc41ac0afbdd
Swathi <swatishetty681@gmail.com>
2026-07-23 18:26:14 +0530

    day-9 JS updates

--- script.js
+++ script.js
@@ -23,3 +23,43 @@ console.log(heroTitle);
 if (heroTitle){
     heroTitle.textContent = `${getGreeting()}, I'm Swathi 👋`;
 }
+
+// ===MOBILE MENU TOGGLE=== 
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click',() => {
+navLinks.classList.toggle('open');
+menuToggle.setAttribute('aria-expanded',navLinks.classList.contains(open));
+})
+
+// ----SCROLL-BASED NAVBAR STYLING---- 
+const header = document.querySelector('.site-header');
+
+window.addEventListener('scroll',() =>{
+    if(window.scrollY> 50){
+        header.classList.add('scrolled')
+    }
+    else{
+        header.classList.remove('scrolled')
+    }
+})
+
+// === ACTIVE NAV LINK ON SCROLL === 
+const sections = document.querySelectorAll('section[id]');
+const navItems = document.querySelectorAll('.nav-links a');
+
+window.addEventListener('scroll', () =>{
+     let current ='';
+    sections.forEach(section =>{
+        if(window.scrollY >= section.offsetTop-100){
+            current=section.getAttribute('id');
+        }
+    })
+    navItems.forEach(link =>{
+        link.classList.remove('active');
+        if(link.getAttribute('href') === `${current}`) {
+            link.classList.add('active');
+        }
+    })
+})
